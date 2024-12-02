df9fef3b7b19f502b50720c2c7809ed6f8a405be
han0110 <tinghan0110@gmail.com>
2024-12-02 08:42:23 +0000

    fix: hashcaster bench

--- hashcaster/benches/hashcaster.rs
+++ hashcaster/benches/hashcaster.rs
@@ -1,23 +1,17 @@
-// use bench::criterion::bench;
-// use bench_binius::hash::{BiniusGroestl, BiniusKeccak};
-// use criterion::{criterion_group, criterion_main, Criterion};
+use bench::{criterion::bench, util::po2};
+use bench_hashcaster::hash::HashcasterKeccak;
+use criterion::{criterion_group, criterion_main, Criterion};
 
-// fn num_perms(logs: impl IntoIterator<Item = usize>) -> impl Iterator<Item = usize> {
-//     logs.into_iter().map(|log| 1 << log)
-// }
+fn bench_keccak(c: &mut Criterion) {
+    let mut group = c.benchmark_group("keccak");
 
-// fn bench_keccak(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("keccak");
+    type H = HashcasterKeccak;
+    bench::<H>(&mut group, "aes_tower_groestl_mt", po2(10..13));
+}
 
-//     type H = BiniusKeccak;
-//     bench::<H>(&mut group, "polyval_groestl_mt", num_perms(10..13));
-// }
-
-// criterion_group!(
-//     name = benches;
-//     config = Criterion::default().sample_size(10);
-//     targets = bench_groestl, bench_keccak,
-// );
-// criterion_main!(benches);
-
-fn main() {}
+criterion_group!(
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = bench_keccak,
+);
+criterion_main!(benches);
