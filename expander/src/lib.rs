e578f8f2112e287f350e50c8221476eeb9114168
han0110 <tinghan0110@gmail.com>
2024-12-03 14:21:22 +0800

    feat: finish `expander`

--- expander/src/lib.rs
+++ expander/src/lib.rs
@@ -1,9 +1,11 @@
 use bench::HashInSnark;
+use expander_arith::FieldSerde;
 use expander_circuit::Circuit;
-use expander_config::{Config, GKRConfig, GKRScheme, MPIConfig};
-use expander_gkr::Prover;
+use expander_config::{Config, FiatShamirHashType, GKRConfig, GKRScheme};
+use expander_gkr::{gkr_verify, Prover};
+use expander_transcript::{BytesHashTranscript, Keccak256hasher, SHA256hasher, Transcript};
 use rand::RngCore;
-use std::cell::RefCell;
+use std::{cell::RefCell, io::Cursor};
 
 pub mod circuit;
 
@@ -17,6 +19,7 @@ pub trait ExpanderCircuit {
 
 pub struct Expander<C: ExpanderCircuit> {
     num_permutations: usize,
+    config: Config<C::Config>,
     circuit: Circuit<C::Config>,
     prover: RefCell<Prover<C::Config>>,
 }
@@ -33,10 +36,12 @@ impl<C: ExpanderCircuit> HashInSnark for Expander<C> {
         let num_permutations = num_permutations.next_power_of_two();
         let circuit_path = format!("{}/{}.txt", C::CIRCUIT_DIR, num_permutations.ilog2());
         let circuit = Circuit::load_circuit(&circuit_path);
-        let mut prover = Prover::new(&Config::new(C::scheme(), MPIConfig::new()));
+        let config = Config::new(C::scheme(), Default::default());
+        let mut prover = Prover::new(&config);
         prover.prepare_mem(&circuit);
         Self {
             num_permutations,
+            config,
             circuit,
             prover: prover.into(),
         }
@@ -53,19 +58,57 @@ impl<C: ExpanderCircuit> HashInSnark for Expander<C> {
     }
 
     fn prove(&self, mut circuit: Self::Input) -> Self::Proof {
-        let (claim, transcript) = self.prover.borrow_mut().prove(&mut circuit);
-        (claim, transcript.bytes)
+        let (claimed_v, transcript) = self.prover.borrow_mut().prove(&mut circuit);
+        (claimed_v, transcript.bytes)
     }
 
-    fn verify(&self, _proof: &Self::Proof) -> Result<(), Self::Error> {
-        todo!()
+    fn verify(&self, (claimed_v, proof): &Self::Proof) -> Result<(), Self::Error> {
+        match C::Config::FIAT_SHAMIR_HASH {
+            FiatShamirHashType::Keccak256 => {
+                let mut transcript = BytesHashTranscript::<_, Keccak256hasher>::new();
+                let proof = Cursor::new(&proof);
+                gkr_verify(
+                    &self.config,
+                    &self.circuit,
+                    &[],
+                    claimed_v,
+                    &mut transcript,
+                    proof,
+                )
+            }
+            FiatShamirHashType::SHA256 => {
+                let mut transcript = BytesHashTranscript::<_, SHA256hasher>::new();
+                let proof = Cursor::new(&proof);
+                gkr_verify(
+                    &self.config,
+                    &self.circuit,
+                    &[],
+                    claimed_v,
+                    &mut transcript,
+                    proof,
+                )
+            }
+            _ => unreachable!(),
+        }
+        .0
+        .then_some(())
+        .ok_or(())
     }
 
-    fn serialize_proof(_proof: &Self::Proof) -> Vec<u8> {
-        todo!()
+    fn serialize_proof((claimed_v, proof): &Self::Proof) -> Vec<u8> {
+        let mut claimed_v_bytes =
+            vec![0; <<C as ExpanderCircuit>::Config as GKRConfig>::ChallengeField::SERIALIZED_SIZE];
+        claimed_v.serialize_into(&mut claimed_v_bytes).unwrap();
+        bincode::serialize(&(claimed_v_bytes, proof)).unwrap()
     }
 
-    fn deserialize_proof(_bytes: &[u8]) -> Self::Proof {
-        todo!()
+    fn deserialize_proof(bytes: &[u8]) -> Self::Proof {
+        let (claimed_v_bytes, proof): (Vec<u8>, _) = bincode::deserialize(bytes).unwrap();
+        let claimed_v =
+            <<C as ExpanderCircuit>::Config as GKRConfig>::ChallengeField::deserialize_from(
+                claimed_v_bytes.as_slice(),
+            )
+            .unwrap();
+        (claimed_v, proof)
     }
 }
